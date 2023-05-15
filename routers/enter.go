40bdeb0bc50a91526c6c2e8c5bd78010210bf65c
wanghanlin <hanlin.wang@raipiot.com>
2023-05-15 13:28:22 +0800

    my gin

---
+++ routers/enter.go
@@ -0,0 +1,92 @@
+package routers
+
+import (
+	_ "gin/docs"
+	"gin/global"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+// router运行
+func Run() error {
+	logrus.Infof("%v-服务启动", global.Config.SystemConfig.Port)
+	return initGin().Run(global.Config.ServerAddress())
+}
+
+// router结构体
+type Routers struct {
+}
+
+// 初始化路由
+func initGin() *gin.Engine {
+	//配置Gin开发环境
+	gin.SetMode(global.Config.Env)
+
+	//路由配置
+	router := gin.Default()
+
+	//配置跨域
+	router.Use(cors())
+
+	//swager
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	//路由组
+	routerGroup := router.Group("")
+
+	//初始化路由体
+	routers := Routers{}
+
+	//系统api
+	routers.SystemRouter(routerGroup)
+	//图片api
+	routers.imagesRouter(routerGroup)
+	//WS系统
+	routers.EchoRouters(routerGroup)
+
+	return router
+}
+
+// 跨域配置
+func cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		method := c.Request.Method
+		origin := c.Request.Header.Get("Origin")
+		// var headerKeys []string
+		// for k, _ := range c.Request.Header {
+		// 	headerKeys = append(headerKeys, k)
+		// }
+		// headerStr := strings.Join(headerKeys, ", ")
+		// if headerStr != "" {
+		// 	headerStr = fmt.Sprintf("access-control-allow-origin, access-control-allow-headers, %s", headerStr)
+		// } else {
+		// 	headerStr = "access-control-allow-origin, access-control-allow-headers"
+		// }
+		if origin != "" {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+			c.Header("Access-Control-Allow-Origin", "*")                                       // 这是允许访问所有域
+			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE,UPDATE") //服务器支持的所有跨域请求的方法,为了避免浏览次请求的多次'预检'请求
+			//  header的类型
+			c.Header("Access-Control-Allow-Headers", "Authorization, Content-Length, X-CSRF-Token, Token,session,X_Requested_With,Accept, Origin, Host, Connection, Accept-Encoding, Accept-Language,DNT, X-CustomHeader, Keep-Alive, User-Agent, X-Requested-With, If-Modified-Since, Cache-Control, Content-Type, Pragma")
+			//              允许跨域设置                                                                                                      可以返回其他子段
+			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers,Cache-Control,Content-Language,Content-Type,Expires,Last-Modified,Pragma,FooBar") // 跨域关键设置 让浏览器可以解析
+			c.Header("Access-Control-Max-Age", "172800")                                                                                                                                                           // 缓存请求信息 单位为秒
+			c.Header("Access-Control-Allow-Credentials", "false")                                                                                                                                                  //  跨域请求是否需要带cookie信息 默认设置为true
+			c.Set("content-type", "application/json")                                                                                                                                                              // 设置返回格式是json
+		}
+
+		//放行所有OPTIONS方法
+		//if method == "OPTIONS" {
+		//    c.JSON(http.StatusOK, "Options Request!")
+		//}
+		if method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		// 处理请求
+		c.Next() //  处理请求
+	}
+}
